9f0ef1b11c3ef7aeb5ff92d12d5cacb8e3d2c333
andry-z <70062506+andry-z@users.noreply.github.com>
2020-10-02 17:07:21 +0200

    Add files via upload
    First upload

---
+++ start/data/geojson.js
@@ -0,0 +1,67 @@
+<<<<<<< HEAD
+export default {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"school"},"geometry":{"type":"Point","coordinates":[16.85575380921364,41.12474418819201]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"hospital"},"geometry":{"type":"Point","coordinates":[16.858649253845215,41.11178978653343]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"rail-above"},"geometry":{"type":"Point","coordinates":[16.867661476135254,41.118514836389764]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"college"},"geometry":{"type":"Point","coordinates":[16.866588592529297,41.120018178788044]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"theatre"},"geometry":{"type":"Point","coordinates":[16.86746835708618,41.12561095674413]}},{"type":"Feature","properties":{"stroke":"#2e073e","stroke-width":2,"stroke-opacity":1,"fill":"#ff2600","fill-opacity":0.5},"geometry":{"type":"Polygon","coordinates":[[[16.870086193084717,41.132544692997186],[16.86452865600586,41.12858495667262],[16.86654567718506,41.125788754006386],[16.8719744682312,41.12620900016575],[16.87371253967285,41.12827786509471],[16.87128782272339,41.130605260168316],[16.871566772460938,41.13235075229427],[16.871588230133057,41.13307802697392],[16.87053680419922,41.133385096084595],[16.869914531707764,41.133336611583715],[16.870086193084717,41.132544692997186]]]}}]}
+=======
+export default {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        name: "La Scuola Open Source",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.855419874191284, 41.12455224488968],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        name: "Officina Degli Esordi",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [16.860194206237793, 41.12532001473043],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Point",
+        coordinates: [16.867704391479492, 41.122960104066],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Point",
+        coordinates: [16.869957447052002, 41.124932090038904],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [16.86573028564453, 41.12113353960666],
+            [16.865880489349365, 41.11843401055425],
+            [16.871867179870605, 41.11883813873656],
+            [16.871609687805176, 41.12150532233404],
+            [16.86573028564453, 41.12113353960666],
+          ],
+        ],
+      },
+    },
+  ],
+};
+>>>>>>> 59e4055f75cb8195281e2dc711a3b9d54025a5f4
